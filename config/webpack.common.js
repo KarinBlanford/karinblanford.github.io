8191fa66e498a8d59470443cf1e9bf4a2c2e7574
Karin Blanford <karinb@email.arizona.edu>
2017-02-22 19:21:01 -0700

    Updating project with webpack configurations -- installing babel for ES6 development

---
+++ config/webpack.common.js
@@ -0,0 +1,10 @@
+// var webpack = require('webpack');
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// var helpers = require('./helpers');
+//
+// entry: {
+//     'polyfills': './src/polyfills.ts',
+//         'vendor': './src/vendor.ts',
+//         'app': './src/main.ts'
+// }
