93c0eae77ff8c24be3009b2169ee0dc2f9f83235
Karin Blanford <karinb@email.arizona.edu>
2017-02-26 11:07:58 -0700

    status update

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,48 @@
-var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    devtool: 'eval-source-map',
+
+    entry:  __dirname + "/src/index.js",
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'parcel.js'
+        path: __dirname + "/dist",
+        filename: "parcel.js"
     },
-    devtool: 'inline-source-map',
+
     module: {
         loaders: [
             {
-                test: path.join(__dirname, 'src'),
-                loader: 'babel-loader'
+                test: /\.json$/,
+                loader: "json"
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
             }
         ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    devServer: {
+        contentBase: "./dist",
+        colors: true,
+        historyApiFallback: true,
+        inline: true
     }
-};
+}
