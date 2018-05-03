d47d26f03e394891f67bca93b64c28921b582971
Karin Blanford <karin@Karins-MacBook-Pro.local>
2018-05-03 03:26:16 -0700

    updating questions for CS Jeopardy

--- www/static-files/Jeopardy.js
+++ www/static-files/Jeopardy.js
@@ -26,193 +26,193 @@ function setQuestionContent(id) {
 
 var master = {
     str: {
-        title: "Strings",
+        title: "Classes",
         100: {
-            question: "What is the index of the 3rd character of the following string?",
-            code: "<code>\"apple\"</code>",
-            answer: "2"
+            question: "Name the method that creates an instance of a class.",
+            code: "",
+            answer: "The constructor"
         },
         200: {
-            question: "What is the output of the following command?",
-            code: "<code>\"Arizona State University\".indexOf(\"Colorado\");</code>",
-            answer: "-1"
+            question: "Fill in the blank. Java is an ______ ________ programming language.",
+            code: "",
+            answer: "object oriented"
         },
         300: {
-            question: "What is the output of the following command?",
-            code: "<code>\"Arizona State University\".indexOf(\"Uni\");</code>",
-            answer: "14"
+            question: "An object has a state and behavior. We call an object an ________ of a class.",
+            code: "",
+            answer: "instance"
         },
         400: {
-            question: "Given the string \"The quick brown fox\", what command would produce the following output?",
-            code: "<samp>ck bro</samp>",
-            answer: "\"The quick brown fox\".substring(7, 13);"
+            question: "What is the ability to derive a new class from an existing one?",
+            code: "",
+            answer: "Inheritance"
         },
         500: {
-            question: "What is the output of the following command?",
-            code: "<code>\"The quick brown fox\".substring(11, 17)</code>",
-            answer: "compilation error: missing semicolon"
+            question: "________ variables define the attributes of an object and maintain the state of the class instance.",
+            code: "",
+            answer: "Instance"
         },
         600: {
-            question: "What is the correct syntax for the condition checking if the length of two strings (str1 and str2) are equal?",
-            code: "<code></code>",
-            answer: "str1.length() == str2.length()"
+            question: "_______ and _______ are used for retreiving and updating the state of an object.",
+            code: "",
+            answer: "Getters and setters"
         },
         700: {
-            question: "GROUP CHALLENGE! A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. Write a function that returns true if a given string is a palindrome.",
-            code: "<code></code>",
+            question: "GROUP CHALLENGE: All teams participate in this group challenge. Create a new class called 'Superhero' that has 3 instance variables: String name, String franchise, and boolean wearsCape. Add and use getter and setter methods for all instance variables as well as an equals method. If the franchise provided via the parameters in the constructor is not 'Marvel' or 'DC' (case sensitive), default the value to 'Marvel'.",
+            code: "",
             answer: ""
         }
     },
     arr: {
-        title: "Arrays",
+        title: "Types",
         100: {
-            question: "What is the correct command for creating a new Integer array of length 10?",
-            code: "<samp></samp>",
-            answer: "int[] arr = new int[10];"
+            question: "A ____ primitive datatype consists of a single character.",
+            code: "",
+            answer: "char"
         },
         200: {
-            question: "After executing this command, what does this array look like?",
-            code: "<samp>String[] arr = new String[4];</samp>",
-            answer: "[null, null, null, null]"
+            question: "A _______ primitive datatype represents a binary value and is often used in conditionals to test or assert the value of an expression.",
+            code: "",
+            answer: "boolean"
         },
         300: {
-            question: "What is the output of the following piece of code?",
-            code: "<code>int[] arr1 = { 1, 2, 3 };</code><br><code>int[] arr2 = { 1, 2, 3 };</code><br><code>System.out.println(arr1 == arr2);</code>",
-            answer: "false"
+            question: "What is the value of c?",
+            code: "char c = \"C\"",
+            answer: "Compilation error: invalid assignment -- cannot convert a string to a char."
         },
         400: {
-            question: "What is the output of the following piece of code?",
-            code: "<code>String[] arr = { \"The\", \"quick\", \"brown\", \"fox\", \"jumped\", \"over\", \"the\", \"lazy\", \"dog.\" };</code><br><code>for(int i = 0; i < arr.length; i+=2)</code><br><code>System.out.println(arr[i].charAt(arr[i].length() - 2));</code>",
-            answer: "hweho"
+            question: "Write a method which accepts two string arrays as parameters and returns true if they are equal.",
+            code: "",
+            answer: ""
         },
         500: {
-            question: "Add another line of code so that each element of the array is equal to itself times the index of the element before it. Write the output.",
-            code: "<code>int[] arr = { 7, 5, 2, 9, 4 };</code><br><code>for(int i = 0; i < arr.length; i++)</code>",
-            answer: "arr[i] = arr[i] * (i - 1);<br>output: -7 0 2 18 12"
+            question: "TRUE or FALSE: the code snippet below is a valid assignment.",
+            code: "<code>int test = 123;<br>String testString = test + \"456\"</code>",
+            answer: "True. Concatenating \"456\" onto 'test' converts the resulting value to a String."
         },
         600: {
-            question: "Complete the given code fragement to reverse any one dimensional integer array.",
-            code: "<samp>for(int i = 0; ________________ ; i++)</samp>",
-            answer: "i < arr.length / 2<br>int temp = arr[i];<br>arr[i] = arr[arr.length - 1 - i];<br>int[arr.length - 1 - i] = temp;"
+            question: "A ______ is actually a Java class represented as an immutable array of chars.",
+            code: "",
+            answer: "String"
         },
         700: {
-            question: "GROUP CHALLENGE! Write out a code fragment that creates a new String array containing the first names of each of your team members. Then add the necessary code to print out the longest name in the array. If there's a tie for the longest name, print that value.",
-            code: "<code></code>",
+            question: "GROUP CHALLENGE: All teams participate in this group challenge. Write a function that takes an array of Strings, each element a full name (First Middle Last) and returns a String array of equal length of each of the names' intials (FML) in uppercase. ",
+            code: "",
             answer: ""
         }
     },
     twodarr: {
-        title: "2D Arrays",
+        title: "Methods",
         100: {
-            question: "Write a line of code to instantiate a 2D integer array with 4 columns and 16 rows.",
-            code: "",
-            answer: "int[][] arr = new int[16][4];"
+            question: "What is the return type of this method?",
+            code: "<code>public boolean isPrime(int n)</code>",
+            answer: "boolean"
         },
         200: {
-            question: "What does the following line of code tell you about an existing 2D array?",
-            code: "<code>System.out.println(arr[2].length);</code>",
-            answer: "Prints the number of columns in the third row."
-        },
-        300: {
-            question: "Complete the given code fragement to reverse any one dimensional integer array.",
-            code: "<samp>for(int i = 0; ________________ ; i++)</samp>",
-            answer: "i < arr.length / 2<br>int temp = arr[i];<br>arr[i] = arr[arr.length - 1 - i];<br>int[arr.length - 1 - i] = temp;"
-        },
-        400: {
-            question: "Write the code fragment to print the following output:",
-            code: "<samp>*</samp><br><samp>**</samp><br><samp>****</samp><br><samp>********</samp><br><samp>****</samp><br><samp>**</samp><br><samp>*</samp>",
-            answer: ""
-        },
-        500: {
-            question: "Write a code fragment to print every other element of every other row of a 2D integer array. Start with the first element of the first row.",
+            question: "In as little code as possible, write a complete method that returns true if its integer parameter is less than 0.",
             code: "",
             answer: ""
         },
+        300: {
+            question: "As efficiently as possible, write a complete method determines if its integer array parameter contains all positive numbers (greater than or equal to 0).",
+            code: "",
+            answer: ""
+        },
+        400: {
+            question: "A class can contain multiple methods with the same method ____ but not the same method signature. This is called method ___________.",
+            code: "",
+            answer: "name -- overloading"
+        },
+        500: {
+            question: "______ _________ restrict the scope of a method.",
+            code: "",
+            answer: "Access modifiers"
+        },
         600: {
-            question: "Assume a 2D character array called 'board' exists with 3 rows and 3 columns. Write an algorithm to determine if 3 characters match in a row (horizontally, vertically, or diagonally).",
+            question: "As efficiently as possible, write a complete method that accepts an integer array of the ages of each member of your team and returns the youngest age.",
             code: "",
             answer: ""
         },
         700: {
-            question: "GROUP CHALLENGE! Write a piece of code which prints every integer in a border position of a 2D integer array. You can assume the array has a minimum of 2 rows and 2 columns.",
-            code: "<code></code>",
+            question: "GROUP CHALLENGE: All teams participate in this group challenge. Write a recursive method called 'factorial' that calculates and returns the value of its integer parameter's factorial i.e. factorial(2) => 2.",
+            code: "",
             answer: ""
         }
     },
     algo: {
-        title: "Algorithms",
+        title: "Concepts",
         100: {
-            question: "Name 3 different sorting algorithms:",
+            question: "____ is a Java term that is used to indicate that a reference does not currently refere to any object.",
             code: "",
-            answer: "selection, bubble, merge, insertion, quick, heap"
+            answer: "null"
         },
         200: {
-            question: "Order the following 3 sorting algorithms in order of best case scenario time: insertion, selection, and merge.",
+            question: "The binary system is comprised of what digits?",
             code: "",
-            answer: "merge, insertion, selection"
+            answer: "1 and 0"
         },
         300: {
-            question: "Write a recursive algorithm in psuedocode to determine if a number is prime.",
+            question: "The java ________ is software that translates developer written software to bytecode that the operating system can understand.",
             code: "",
-            answer: ""
+            answer: "compiler"
         },
         400: {
-            question: "Write an algorithm in psuedocode to find the longest string in a 1D String array.",
+            question: "What is the difference between a compile time and runtime error? Give examples of both.",
             code: "",
-            answer: ""
+            answer: "A compile time error is thrown when..."
         },
         500: {
-            question: "Write an algorithm in psuedocode to find the largest number in a 2D integer array.",
+            question: "An ________ class cannot be instantiated but can be subclassed using the extends keyword.",
             code: "",
-            answer: ""
+            answer: "abstract"
         },
         600: {
-            question: "Insertion sort and selection sort are similar algorithms to sort a set of elements. How are they different?",
-            code: "",
-            answer: "Both maintain an ordered and unordered array. Selection sort searches the whole array for the next smallest element and places it in its sorted place while selection sort uses the next element in the unsorted array and comparing it against each element in the sorted array, inserts it in its right place."
+            question: "What is the output of the following code?",
+            code: "<code>ArrayList names;<br>names.add(\"John\");<br>System.out.println(names.get(0));</code>",
+            answer: ""
         },
         700: {
-            question: "GROUP CHALLENGE! Write the algorithm for selection sort.",
+            question: "GROUP CHALLENGE: All teams participate in this group challenge. Write a COMPLETE main method that asks a user to enter the correct password. The password is 'abc123'. If the user guesses the password incorrectly, continually ask them until they get it right. Print 'Login successful' to the console once the user enters the correct password.",
             code: "",
-            answer: "<samp>Step 1: Set MIN to location 0</samp><br><samp>Step 2: Search the minimum element in the list</samp><br><samp>Step 3: Swap with value at location MIN</samp><br><samp>Step 4: Increment MIN to point to next element</samp><br><samp>Step 5: Repeat until list is sorted</samp>"
+            answer: ""
         }
     },
     cs: {
-        title: "CS Topics",
+        title: "Java Math",
         100: {
-            question: "Name one job career path or job title you could have after studying computer science.",
-            code: "<code></code>",
+            question: "What is the value of the following expression?",
+            code: "<code>int a = 5;<br>int b = 2;<br>System.out.println(a/b);</code>",
             answer: ""
         },
         200: {
-            question: "What is the name of the most important (and unsolved) problem in computer science?",
-            code: "<code></code>",
-            answer: "P vs NP"
-        },
-        300: {
-            question: "Name one of the encryption methods we talked about in class.",
-            code: "<code></code>",
-            answer: "Symmetric key, public key"
-        },
-        400: {
-            question: "Name one of the assertion methods used in Java JUnit testing.",
-            code: "<code></code>",
-            answer: "AssertEquals, AssertTrue, AssertFalse"
-        },
-        500: {
-            question: "Name one famous computer scientist.",
-            code: "<code></code>",
+            question: "Write a function called 'isFactor' that takes two integer parameters k and n and returns true if k is a factor of n.",
+            code: "",
             answer: ""
         },
-        600: {
-            question: "Name 6 different java primitive types:",
+        300: {
+            question: "Write a function called 'xor' that takes two boolean parameters and returns true if exactly one of the parameters is true but not both.",
             code: "",
-            answer: "boolean, character, integer, decimal, float, byte, short, long, double"
+            answer: ""
+        },
+        400: {
+            question: "What is the value of the following expression?",
+            code: "<code>System.out.println( 5 / 2 + 4 - 3 * 7 );</code>",
+            answer: "-15"
+        },
+        500: {
+            question: "What is the value of the following expression?",
+            code: "<code>System.out.println( 5 / 2 + (4 - 3) * 7 );</code>",
+            answer: "9"
+        },
+        600: {
+            question: "What is the output of the following code snippet?",
+            code: "<code>System.out.println( \"apple\".indexOf(\"ple\") - \"orange\".length() / \"banana\".indexOf(\"an\") * \"pineapple\".substring(4).indexOf(\"ple\") + \"pineapples\".length() );</code>",
+            answer: "0"
         },
         700: {
-            question: "What is the name of the property of an object or type that dictates that it cannot be changed once it has been created?",
-            code: "<code></code>",
-            answer: "immutable or immutability"
+            question: "GROUP CHALLENGE: All teams participate in this group challenge. Write a function called 'reverseNum' that accepts an integer as an argument and returns that integer as a String with its digits in reverse order. You can assume that all numbers tested with this function will be between 100 and 999 inclusive.",
+            code: "",
+            answer: ""
         }
     }
 };
